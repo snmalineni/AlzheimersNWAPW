f746670eca01ca8d19488705d0a43f8d390af0c9
Savannah Bowerfind <savannahbowerfind@Savannahs-MacBook-Pro-3.local>
2016-08-01 13:47:32 -0700

    intial layout for library - 6 imageviews

---
+++ app/src/main/java/com/sneha/newalzheimersapplication/Library.java
@@ -0,0 +1,17 @@
+package com.sneha.newalzheimersapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class Library extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_library);
+
+    }
+
+
+}
