50f608e12b3f4d4a3f282117c6b68795d63817ca
Savannah Bowerfind <savannahbowerfind@Savannahs-MacBook-Pro-3.local>
2016-08-03 11:27:04 -0700

    tabhost is dead, buttons rule the world

--- app/src/main/java/com/sneha/newalzheimersapplication/Homepage.java
+++ app/src/main/java/com/sneha/newalzheimersapplication/Homepage.java
@@ -1,13 +1,16 @@
 package com.sneha.newalzheimersapplication;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import android.view.View;
 import 	android.content.Intent;
+import android.widget.TabHost;
 
 public class Homepage extends AppCompatActivity {
 
+    TabHost tabHost;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,5 +31,10 @@ public class Homepage extends AppCompatActivity {
         Intent intent = new Intent(this, Tutorial.class);
         startActivity(intent);
     } //when Tutorial button pressed, go to Tutorial page
+
+    public void goLib(View view) {
+        Intent intent = new Intent(this, Library.class);
+        startActivity(intent);
+    }
 
 }
