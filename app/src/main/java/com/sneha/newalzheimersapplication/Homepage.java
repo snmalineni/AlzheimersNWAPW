9c3d5806272894459693a9cbeb7ab9f674ba360b
savannah-b <sbowerfind@gmail.com>
2016-08-05 09:50:32 -0700

    Revert "Development"

--- app/src/main/java/com/sneha/newalzheimersapplication/Homepage.java
+++ app/src/main/java/com/sneha/newalzheimersapplication/Homepage.java
@@ -6,17 +6,11 @@ import android.os.Bundle;
 
 import android.view.View;
 import 	android.content.Intent;
-import android.R.id;
-
-
-import java.util.ArrayList;
-
-import Source.cappedPhoto;
+import android.widget.TabHost;
 
 public class Homepage extends AppCompatActivity {
 
-    public static ArrayList<cappedPhoto> photoAr = new ArrayList(50);
-
+    TabHost tabHost;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,6 +35,6 @@ public class Homepage extends AppCompatActivity {
     public void goLib(View view) {
         Intent intent = new Intent(this, Library.class);
         startActivity(intent);
-} //when Tutorial button pressed, go to Tutorial page (TEMPORARY)
+    }
 
 }
