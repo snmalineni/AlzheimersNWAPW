bb783fe61b7b003e41dcbef27fe9c7d86418bccb
Savannah Bowerfind <savannahbowerfind@Savannahs-MacBook-Pro-3.local>
2016-08-08 13:57:52 -0700

    game class set for randomization

--- app/src/main/java/Source/Question.java
+++ app/src/main/java/Source/Question.java
@@ -2,7 +2,10 @@ package Source;
 
 import android.net.Uri;
 
+import com.sneha.newalzheimersapplication.Homepage;
+
 import static android.net.Uri.parse;
+import java.util.Random;
 
 public class Question {
     private int ID;
@@ -14,21 +17,41 @@ public class Question {
 
     public Question() {
         ID=0;
-        imgUri="";
-        opta="";
-        optb="";
-        optc="";
-        ans="";
+        imgUri=null;
+        opta=null;
+        optb=null;
+        optc=null;
+        ans=null;
     }
 
-    public Question(String iMGURI, String oPTA, String oPTB, String oPTC,
-                    String aNSWER) {
+    public Question(cappedPhoto cPhoto) {
+        imgUri = cPhoto.getUriString();
+        ans = cPhoto.getCap();
+        opta = null;
+        optb = null;
+        optc = null;
 
-        imgUri = iMGURI;
-        opta = oPTA;
-        optb = oPTB;
-        optc = oPTC;
-        ans = aNSWER;
+        Random rn = new Random();
+        int arLength = Homepage.photoAr.size();
+        int r = rn.nextInt(arLength);
+        cappedPhoto rCP;
+        //gets random number bounded by size of photoAr
+
+        do {
+            rCP = Homepage.photoAr.get(r);
+            opta = rCP.getCap();
+            //sets opta to random cappedPhoto caption
+        } while (opta.compareTo(ans) != 0);
+        do {
+            rCP = Homepage.photoAr.get(r);
+            optb = rCP.getCap();
+            //sets optb to random cappedPhoto caption
+        } while (optb.compareTo(ans) != 0);
+        do {
+            rCP = Homepage.photoAr.get(r);
+            optc = rCP.getCap();
+            //sets optc to random cappedPhoto caption
+        } while (optc.compareTo(ans) != 0);
     }
     public int getID() {return ID;}
     public Uri getimgUri() {
@@ -41,8 +64,5 @@ public class Question {
 
     public void setID(int id) {ID=id;}
     public void setimgUri(String iMGURI) {imgUri = iMGURI;}
-    public void setopta(String oPTA) {opta = oPTA;}
-    public void setoptb(String oPTB) {optb = oPTB;}
-    public void setoptc(String oPTC) {optc = oPTC;}
     public void setans(String aNSWER) {ans = aNSWER;}
 }
--- app/src/main/java/Source/cappedPhoto.java
+++ app/src/main/java/Source/cappedPhoto.java
@@ -9,25 +9,23 @@ import android.net.Uri;
 import java.io.Serializable;
 
 public class cappedPhoto implements Serializable {
-private String caption;
-private String photo;
+    private String caption;
+    private String photo;
 
- public String getCap(){
+    public String getCap(){
      return caption;
  }
- public String getUriString(){
+    public String getUriString(){
      return photo;
  }
- public cappedPhoto(String b, String str)
-        {
+    public cappedPhoto(String b, String str) {
         caption = new String(str);
         photo = new String(b);
-        }
+    }
 
 
-public Uri stringToUri (String a)
-        {
+    public Uri stringToUri (String a) {
         Uri u = Uri.parse(a);
         return u;
-        }
-        } //Object for photo Uri and caption
+    }
+} //Object for photo Uri and caption
