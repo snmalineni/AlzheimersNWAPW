a69baee606047f30aa06ef6f8e8790d568d3244a
Savannah Bowerfind <savannahbowerfind@Savannahs-MacBook-Pro-3.local>
2016-08-08 14:29:40 -0700

    game xml outlined

--- app/src/main/java/com/sneha/newalzheimersapplication/Game.java
+++ app/src/main/java/com/sneha/newalzheimersapplication/Game.java
@@ -2,6 +2,8 @@ package com.sneha.newalzheimersapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
 
 public class Game extends AppCompatActivity {
 
@@ -10,4 +12,7 @@ public class Game extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
     }
+
+    Button btnA = (Button)findViewById(R.id.A);
+    //btnA.setText();
 }
