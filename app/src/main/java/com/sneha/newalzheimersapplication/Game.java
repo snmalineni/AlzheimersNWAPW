6a3a4db4d3131a821df77226f7a0b29e36860424
Savannah Bowerfind <savannahbowerfind@Savannahs-MacBook-Pro-3.local>
2016-08-09 10:59:39 -0700

    start button added to game

--- app/src/main/java/com/sneha/newalzheimersapplication/Game.java
+++ app/src/main/java/com/sneha/newalzheimersapplication/Game.java
@@ -2,22 +2,39 @@ package com.sneha.newalzheimersapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RemoteViews;
+import android.widget.TextView;
 
 public class Game extends AppCompatActivity {
 
     private ImageView imgView = (ImageView) findViewById(R.id.imgView);
+    private TextView myName = (TextView) findViewById(R.id.myName);
+    private Button btnA = (Button) findViewById(R.id.A);
+    private Button btnB = (Button) findViewById(R.id.B);
+    private Button btnC = (Button) findViewById(R.id.C);
+    private Button btnD = (Button) findViewById(R.id.D);
+    private Button btnStart = (Button) findViewById(R.id.btnStart);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-        //grab the layout, then set the text of the Button called R.id.Counter:
+        myName.setVisibility(View.GONE);
+        btnA.setVisibility(View.GONE);
+        btnB.setVisibility(View.GONE);
+        btnC.setVisibility(View.GONE);
+        btnD.setVisibility(View.GONE);
+    }
+
+
+    public void startGame(View view){
+        btnStart.setVisibility(View.GONE);
+
         RemoteViews remoteViews = new RemoteViews(getPackageName(), R.layout.activity_game);
         remoteViews.setTextViewText(R.id.A, "Set button text here");
     }
-
 }
