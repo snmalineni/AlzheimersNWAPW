e98949830ba84a15373d7b98eb6e2134e9476d89
Savannah Bowerfind <savannahbowerfind@Savannahs-MacBook-Pro-3.local>
2016-08-09 14:13:16 -0700

    homepage to blank game blocked

--- app/src/main/java/com/sneha/newalzheimersapplication/Homepage.java
+++ app/src/main/java/com/sneha/newalzheimersapplication/Homepage.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 import android.view.View;
 import 	android.content.Intent;
+import android.widget.Toast;
 
 
 import java.util.ArrayList;
@@ -14,7 +15,7 @@ import Source.cappedPhoto;
 
 public class Homepage extends AppCompatActivity {
 
-    public static ArrayList<cappedPhoto> photoAr = new ArrayList(50);
+    public static ArrayList<cappedPhoto> photoAr = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,26 +25,42 @@ public class Homepage extends AppCompatActivity {
 
     public void goInfo(View view) {
         Intent intent = new Intent(this, AlzInformation.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(intent);
     } //when Alz Info button pressed, go to Alz Info page
 
     public void goHelp(View view) {
         Intent intent = new Intent(this, AlzHelp.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(intent);
     } //when Emergency Help button pressed, go to Help page
 
     public void goTutorial(View view) {
         Intent intent = new Intent(this, Tutorial.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(intent);
     } //when Tutorial button pressed, go to Tutorial page
 
     public void goLib(View view) {
         Intent intent = new Intent(this, Library.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+    }
+
+
+    public void goUpload(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(intent);
     }
 
     public void goGame(View view) {
-        Intent intent = new Intent(this, Game.class);
-        startActivity(intent);
+        if (Homepage.photoAr != null) {
+            Intent intent = new Intent(this, Game.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+        } else {
+            Toast.makeText(getApplicationContext() , "Upload some images first!", Toast.LENGTH_LONG).show();
+        }
     }
 }
