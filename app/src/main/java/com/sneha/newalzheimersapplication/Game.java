9f5289819104c6e9de24134715a82f31167385c4
Savannah Bowerfind <savannahbowerfind@Savannahs-MacBook-Pro-3.local>
2016-08-09 10:48:06 -0700

    save before making game dynamic

--- app/src/main/java/com/sneha/newalzheimersapplication/Game.java
+++ app/src/main/java/com/sneha/newalzheimersapplication/Game.java
@@ -3,16 +3,21 @@ package com.sneha.newalzheimersapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
-
+import android.widget.ImageView;
+import android.widget.RemoteViews;
 
 public class Game extends AppCompatActivity {
+
+    private ImageView imgView = (ImageView) findViewById(R.id.imgView);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        //grab the layout, then set the text of the Button called R.id.Counter:
+        RemoteViews remoteViews = new RemoteViews(getPackageName(), R.layout.activity_game);
+        remoteViews.setTextViewText(R.id.A, "Set button text here");
     }
 
-    Button btnA = (Button)findViewById(R.id.A);
-    //btnA.setText();
 }
